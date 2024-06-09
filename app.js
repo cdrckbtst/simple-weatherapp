82d61f8d2705861869a2f4d4c464f149c1b16f06
cdrckbtst <159131948+cdrckbtst@users.noreply.github.com>
2024-06-09 20:38:24 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,65 +1,78 @@
-const apiURL = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
-const apiKey = "8781523e8c4370b7a1e4e5659336946e";
-const icon = document.querySelector(".weather-icon");
-const input = document.querySelector(".search input");
+const apiKey = "8781523e8c4370b7a1e4e5659336946e"
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
+const searchInput = document.querySelector(".search input");
 const button = document.querySelector(".search button");
+const icon = document.querySelector(".weather-icon");
 
-const checkWeather = async function (location) {
+const updateWeather = async function (location) {
     try {
-        const res = await axios.get(apiURL + location + `&appid=${apiKey}`)
-
+        const res = await axios.get(`${apiURL}${location}&appid=${apiKey}`)
+        console.log(res.data)
         if (res.status === 404) {
             document.querySelector(".error").style.display = "block"
-            document.querySelector(".weather").style.display = "none";
+            document.querySelector(".weather").style.display = "none"
         } else {
-            document.querySelector(".weather").style.display = "block";
-            document.querySelector(".error").style.display = "none";
+            document.querySelector(".city").textContent = res.data.name;
+            document.querySelector(".temp").textContent = Math.round(res.data.main.temp) + "°C";
+            document.querySelector(".humidity").textContent = res.data.main.humidity + "%";
+            document.querySelector(".wind").textContent = res.data.wind.speed + " km/h";
 
-            document.querySelector(".city").innerHTML = res.data.name;
-            document.querySelector(".temp").innerHTML = Math.round(res.data.main.temp) + "°C";
-            document.querySelector(".humidity").innerHTML = res.data.main.humidity + "%";
-            document.querySelector(".wind").innerHTML = res.data.wind.speed + "km/h";
-            console.log(res.data)
-
-            if (res.data.weather[0].main === "Clouds") {
-                icon.src = "./images/clouds.png"
-            } else if (res.data.weather[0].main === "Drizzle") {
-                icon.src = "./images/drizzle.png"
-            } else if (res.data.weather[0].main === "Ash") {
-                icon.src = "./images/ash.png"
-            } else if (res.data.weather[0].main === "Snow") {
-                icon.src = "./images/snow.png"
-            } else if (res.data.weather[0].main === "Thunderstorm") {
-                icon.src = "./images/tstorm.png"
-            } else if (res.data.weather[0].main === "Clear") {
-                icon.src = "./images/clear.png"
-            } else if (res.data.weather[0].main === "Tornado") {
-                icon.src = "./images/tornado.png"
-            } else if (res.data.weather[0].main === "Rain" || res.data.weather[0].main === "Squall") {
-                icon.src = "./images/rain.png"
-            } else if (res.data.weather[0].main === "Mist" || res.data.weather[0].main === "Haze" || res.data.weather[0].main === "Fog" || res.data.weather[0].main === "Smoke") {
-                icon.src = "./images/mist.png"
-            } else if (res.data.weather[0].main === "Sand" || res.data.weather[0].main === "Dust") {
-                icon.src = "./images/sand.png"
+            let iconSrc;
+            switch (res.data.weather[0].main) {
+                case "Clouds":
+                    iconSrc = "./images/clouds.png"
+                    break;
+                case "Drizzle":
+                    iconSrc = "./images/drizzle.png"
+                    break;
+                case "Ash":
+                    iconSrc = "./images/ash.png"
+                    break;
+                case "Snow":
+                    iconSrc = "./images/snow.png"
+                    break;
+                case "Thunderstorm":
+                    iconSrc = "./images/tstorm.png"
+                    break;
+                case "Clear":
+                    iconSrc = "./images/clear.png"
+                    break;
+                case "Tornado":
+                    iconSrc = "./images/tornado.png"
+                    break;
+                case "Rain":
+                case "Squall":
+                    iconSrc = "./images/rain.png"
+                    break;
+                case "Sand":
+                case "Dust":
+                    iconSrc = "./images/sand.png"
+                    break;
+                case "Mist":
+                case "Haze":
+                case "Fog":
+                case "Smoke":
+                    iconSrc = "./images/mist.png"
+                    break;
             }
+            icon.src = iconSrc
 
-            input.value = "";
-
+            document.querySelector(".error").style.display = "none"
+            document.querySelector(".weather").style.display = "block"
+            searchInput.value = "";
         }
     } catch (e) {
-        console.error(e)
         document.querySelector(".error").style.display = "block"
-        document.querySelector(".weather").style.display = "none";
+        document.querySelector(".weather").style.display = "none"
+        console.log(e)
+        searchInput.value = "";
     }
 }
 
 button.addEventListener("click", function () {
-    checkWeather(input.value)
+    updateWeather(searchInput.value)
 })
 
-// (rain,squall) (Haze Mist Fog Smoke) (Sand Dust)
-//Clouds Drizzle Ash Snow Thunderstorm Clear Tornado
-
-// https://api.openweathermap.org/data/2.5/weather?q=balanga&appid=8781523e8c4370b7a1e4e5659336946e&units=metric
 
 
+// clouds, drizzle, ash, snow, thunderstorm, clear, tornado, (rain, squall), (mist, haze, fog, smoke), (sand,dust)
